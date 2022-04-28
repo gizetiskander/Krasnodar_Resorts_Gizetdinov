adf415acd3c7bdbc5f642e986988238a40c18a9e
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-04-28 18:33:49 +0300

    RegPage FrontEnd

--- Krasnodar_Resorts_Gizetdinov/ViewPages/PagesFoundation.cs
+++ Krasnodar_Resorts_Gizetdinov/ViewPages/PagesFoundation.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Krasnodar_Resorts_Gizetdinov.ViewPages
 {
-    class PagesFoundation
+    internal abstract class PagesFoundation : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void onPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            onPropertyChanged(PropertyName);
+            return true;
+        }
     }
 }
