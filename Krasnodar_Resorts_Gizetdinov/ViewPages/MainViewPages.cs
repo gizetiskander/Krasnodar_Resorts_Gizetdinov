e18cc1665c51c2a3255040d0ba01813b1560cb20
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-04-28 18:55:26 +0300

    Mvvm added

--- Krasnodar_Resorts_Gizetdinov/ViewPages/MainViewPages.cs
+++ Krasnodar_Resorts_Gizetdinov/ViewPages/MainViewPages.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using GalaSoft.MvvmLight.Command;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using Krasnodar_Resorts_Gizetdinov.Pages;
+using System.Windows.Input;
 
 namespace Krasnodar_Resorts_Gizetdinov.ViewPages
 {
-    class MainViewPages
+    internal class MainViewPages : PagesFoundation
     {
+        private Page MainPage = new MainPage();
+        private Page OrderPage = new OrderPage();
+        private Page SalePage = new SalePage();
+        private Page InfoPage = new InfoPage();
+        private Page PersonalPage = new PersonalPage();
+        private Page _CurPage = new MainPage();
+
+        public Page CurPage
+        {
+            get => _CurPage;
+            set => Set(ref _CurPage, value);
+        }
+
+        public ICommand OpenMPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = MainPage);
+            }
+        }
+
+        public ICommand OpenOPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = OrderPage);
+            }
+        }
+
+        public ICommand OpenSPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = SalePage);
+            }
+        }
+
+        public ICommand OpenIPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = InfoPage);
+            }
+        }
+
+        public ICommand OpenPPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = PersonalPage);
+            }
+        }
     }
 }
